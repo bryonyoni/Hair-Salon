9f9df8f4e278a951f7fe3bc0457f63743bb5387a
bryonyoni <bryonyoni@gmail.com>
2017-05-06 20:15:39 +0300

    Added a save and find method
    added a save method that saves a stylists name and a find method  that
    finds a Stylist from the database.

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -1,25 +1,72 @@
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import org.sql2o.*;
 
 public class Stylist{
   private String name;
-  private String email;
   private String image;
   private int id;
 
 
- // @Override
- //  public boolean equals(Object otherStylists) {
- //  if (!(otherStylists instanceof Stylists)) {
- //  return false;
- //  } else {
- //    Stylists newStylists = (Stylists) otherStylists;
- //     return this.getName().equals(newStylists.getName()) &&
- //     this.getId() == newStylists.getId();
- //    }
- //  }
+ @Override
+  public boolean equals(Object otherStylist) {
+  if (!(otherStylist instanceof Stylist)) {
+  return false;
+  } else {
+    Stylist newStylist = (Stylist) otherStylist;
+     return this.getName().equals(newStylist.getName()) &&
+     this.getImage().equals(newStylist.getImage());
+    }
+  }
+
+  //constructor
+    public  Stylist(String name, String image){
+      this.name = name;
+      this.image = image;
+    }
+  //
+
+  public String getName(){
+    return name;
+  }
+
+  public String getImage(){
+    return image;
+  }
+
+  public int getId(){
+    return id;
+  }
+
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO stylists(name,image) VALUES (:name,:image)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .addParameter("image",this.image)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+
+  public static List<Stylist> all(){
+    String sql = "SELECT id, name, image FROM stylists";
+    try(Connection con = DB.sql2o.open()) {
+     return con.createQuery(sql).executeAndFetch(Stylist.class);
+    }
+  }
+
+  public static Stylist find(int id) {
+   try(Connection con = DB.sql2o.open()){
+     String sql = "SELECT * FROM stylists where id =:id";
+     Stylist stylist = con.createQuery(sql)
+     .addParameter("id",id)
+     .executeAndFetchFirst(Stylist.class);
+    return stylist;
+   }
+  }
 
 
 }
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -8,6 +8,73 @@ public class StylistTest{
   public DatabaseRule database = new DatabaseRule();
 
 
+  @Test
+  public void Stylist_instantiatesCorrectly_true() {
+    Stylist testStylist = new Stylist("name","image");
+    assertEquals(true, testStylist instanceof Stylist);
+  }
+
+  @Test
+  public void getName_stylistInstantiatesWithName_name() {
+      Stylist testStylist = new Stylist("name","image");
+    assertEquals("name", testStylist.getName());
+  }
+
+  @Test
+  public void getImage_stylistInstantiatesWithImage_image() {
+      Stylist testStylist = new Stylist("name","image1");
+    assertEquals("image1", testStylist.getImage());
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfStylist_true() {
+    Stylist firstStylist = new Stylist("name","image1");
+    firstStylist.save();
+    Stylist secondStylist = new Stylist("name","image1");
+    secondStylist.save();
+    assertEquals(true, Stylist.all().get(0).equals(firstStylist));
+    assertEquals(true, Stylist.all().get(1).equals(secondStylist));
+  }
+
+  @Test
+  public void getId_stylistInstantiateWithAnId_1() {
+    Stylist testStylist = new Stylist("name","image1");
+    testStylist.save();
+    assertTrue(testStylist.getId() > 0);
+  }
+
+  @Test
+  public void save_savesIntoDatabase_true() {
+    Stylist myStylist = new Stylist("name","image1");
+    myStylist.save();
+    assertEquals("name",Stylist.all().get(0).getName());
+  }
+
+  @Test
+  public void equals_returnsTrueIfNamesAretheSame_true() {
+    Stylist firstStylist = new Stylist("name","image1");
+    Stylist secondStylist = new Stylist("name","image1");
+    assertTrue(firstStylist.equals(secondStylist));
+  }
+
+  @Test
+  public void save_assignsIdToObjectBeingSaved_1() {
+    Stylist myStylist = new Stylist("name","image1");
+    myStylist.save();
+    Stylist savedStylist = Stylist.all().get(0);
+    assertEquals(1, savedStylist.getId());
+  }
+
+  @Test
+  public void find_returnsStylistWithSameId_secondStylist() {
+    Stylist firstStylist = new Stylist("name","image1");
+    firstStylist.save();
+    Stylist secondStylist = new Stylist("name","image1");
+    secondStylist.save();
+    assertEquals(Stylist.find(secondStylist.getId()), secondStylist);
+  }
+
+
 
 
 }
