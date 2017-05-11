ae2b4f978ec92c3077f4f9ccee81c3b4fdeb2257
bryonyoni <bryonyoni@gmail.com>
2017-05-11 16:00:17 +0300

    Added a route to display names of new stylists
    Added a page to sign up to become a new stylist and to view stylists.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,8 +10,6 @@ public class App{
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
-
-
     get("/", (request,response) -> {
       Map<String,Object> model = new HashMap<String,Object>();
       model.put("Stylist",Stylist.all());
@@ -19,6 +17,31 @@ public class App{
       model.put("template","templates/index.vtl");
       return new ModelAndView(model,layout);
     },new VelocityTemplateEngine());
+
+    get("/stylists",(request,response) ->{
+      Map<String,Object> model = new HashMap<String,Object>();
+      model.put("stylists",Stylist.all());
+      model.put("template","templates/stylists.vtl");
+      return new ModelAndView(model,layout);
+    },new VelocityTemplateEngine());
+
+    get("/stylists/new",(request,response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template","templates/stylist-form.vtl");
+      return new ModelAndView(model,layout);
+    },new VelocityTemplateEngine());
+
+    post("/stylists/success",(request,response) -> {
+      Map<String,Object> model = new HashMap<String,Object>();
+      String name = request.queryParams("name");
+      String image = request.queryParams("image");
+      Stylist newStylist = new Stylist(name,image);
+      newStylist.save();
+      model.put("template","templates/stylist-success.vtl");
+      return new ModelAndView(model,layout);
+    },new VelocityTemplateEngine());
+
+
   }
 
 
