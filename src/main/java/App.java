898752da414ef6fd6f69effb9221d34bced4ebe9
bryonyoni <bryonyoni@gmail.com>
2017-05-11 21:24:05 +0300

    Added a route to render the details of the stylist
    Added a route that renders the info about the stylists and her clients

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -41,6 +41,14 @@ public class App{
       return new ModelAndView(model,layout);
     },new VelocityTemplateEngine());
 
+    get("/stylists/:id",(request,response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Stylist stylist = Stylist.find(Integer.parseInt(request.params(":id")));
+      model.put("stylist",stylist);
+      model.put("template","templates/stylist.vtl");
+      return new ModelAndView(model,layout);
+    }, new VelocityTemplateEngine());
+
 
   }
 
